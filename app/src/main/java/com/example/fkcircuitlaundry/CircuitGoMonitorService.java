f3114cb17586f00e69d5d82f82102922b49c0130
Gwilym-Rutherford <gmarutherford1@sheffield.ac.uk>
2026-02-09 13:34:45 +0000

    init

---
+++ app/src/main/java/com/example/fkcircuitlaundry/CircuitGoMonitorService.java
@@ -0,0 +1,49 @@
+package com.example.fkcircuitlaundry;
+
+import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
+
+public class CircuitGoMonitorService extends AccessibilityService {
+
+    private static final String TAG = "CircuitGoMonitorService";
+    private boolean wifiDisabled = false;
+
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.d(TAG, "onAccessibilityEvent: " + event);
+        if(event.getPackageName() != null && event.getPackageName().equals("co.uk.circuit.go.app")){
+            android.util.Log.d("APP", "Circuit Go App event captured!");
+
+            AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+            if (rootNode != null) {
+                List<AccessibilityNodeInfo> foundNodes = rootNode.findAccessibilityNodeInfosByText("Choose machine");
+
+                for (AccessibilityNodeInfo node : foundNodes) {
+                    disableWifi();
+                }
+                rootNode.recycle();
+            }
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+
+    private void disableWifi() {
+        if(!wifiDisabled) {
+            Intent panelIntent = new Intent(android.provider.Settings.Panel.ACTION_INTERNET_CONNECTIVITY);
+            panelIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(panelIntent);
+            wifiDisabled = true;
+        }
+    }
+}
